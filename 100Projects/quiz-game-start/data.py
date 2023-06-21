5cb671a69ad7c3f7d579c2d6f9746a0fcdacabd4
KaustubhK03 <98525109+KaustubhK03@users.noreply.github.com>
2023-06-21 20:35:35 +0530

    Add files via upload

---
+++ 100Projects/quiz-game-start/data.py
@@ -0,0 +1,115 @@
+question_data = [
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Coca-Cola&#039;s original colour was green.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The vapor produced by e-cigarettes is actually water.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "An eggplant is a vegetable.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Cucumbers are usually more than 90% water.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "You are allowed to sell your soul on eBay.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The average woman is 5 inches or 13 centimeters shorter than the average man.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The term &quot;Spam&quot; came before the food product &quot;Spam&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Sitting for more than three hours a day can cut two years off a person&#039;s life expectancy.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The French word for &quot;glass&quot; is &quot;glace&quot;.", "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Fast food restaurant chains Carl&#039;s Jr. and Hardee&#039;s are owned by the same company.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "&quot;Santa Claus&quot; is a variety of melon.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Haggis is traditionally ate on Burns Night.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The commercial UK channel ITV stands for &quot;International Television&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The pickled gherkin was first added to hamburgers because a US health law required all fast-food "
+                    "to include a source of Vitamin C.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "&quot;Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo.&quot; is a grammatically "
+                    "correct sentence.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
